e6ff426bb6c9e4a54fce2bd31b9d1fdf4f6d6810
AldiRachmatdiantoo <aldirachmatdianto7@gmail.com>
2025-11-08 13:48:09 +0700

    set index route

---
+++ app/controllers/BookController.php
@@ -0,0 +1,9 @@
+<?php
+class BookController {
+    private $model;
+
+    public function index(){
+        include_once __DIR__ . "/../views/index.php";
+    }
+}
+?>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
-echo "tes";
+require_once __DIR__ . "/../app/controllers/BookController.php";
+
+$action = $_GET['action'] ?? "index";
+$controller = new BookController();
+
+switch ($action) {
+    case "index": $controller->index();break;
+}
 ?>
